da044a1197ab5d6c39e31707ffe73ae005a92b10
Lomig <dunwael@gmail.com>
2020-02-11 11:16:32 +0100

    Task Model created

---
+++ db/migrate/20200211100854_create_tasks.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+##
+# Migration class - Initial creation of Task model
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
