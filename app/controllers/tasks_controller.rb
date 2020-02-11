a93daed0fa369f4450416ce32388a877499255f6
Lomig <dunwael@gmail.com>
2020-02-11 14:23:04 +0100

    Complete without CSS nor checkbox toggle

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@
 ##
 # Task Controller
 class TasksController < ApplicationController
-  before_action :find_task, only: [:show]
+  before_action :find_task, only: %i[edit destroy show update]
 
   def index
     @tasks = Task.all
@@ -15,9 +15,34 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.create(task_params)
+    task.save
+
+    redirect_to task_path(task)
+  end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
   private
 
   def find_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@
 
 Rails.application.routes.draw do
   get "tasks" => "tasks#index"
-  get "tasks/:id" => "tasks#show", as: :task
+
   get "tasks/new" => "tasks#new"
+  post "tasks" => "tasks#create"
+
+  get "tasks/:id/edit" => "tasks#edit", as: :task_edit
+  patch "tasks/:id" => "tasks#update"
+
+  get "tasks/:id" => "tasks#show", as: :task
+
+  delete "tasks/:id" => "tasks#destroy", as: :task_destroy
 end
