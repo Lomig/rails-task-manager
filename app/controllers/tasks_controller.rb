8807a1f39373a4ccfba0df0c974ce0897197177f
Lomig <dunwael@gmail.com>
2020-02-11 12:17:26 +0100

    Read views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
+##
+# Task Controller
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "tasks" => "tasks#index"
+  get "tasks/:id" => "tasks#show", as: :task
+  get "tasks/new" => "tasks#new"
 end
